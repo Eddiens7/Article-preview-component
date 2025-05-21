cfed7357e48dc2f933db37b0f19ff344f19d94e5
Edmon Nascimento <eddie@60s1pt.onmicrosoft.com>
2025-05-21 16:26:26 -0300

    mobile version

---
+++ script.js
@@ -0,0 +1,10 @@
+function showHide(){
+    const share = document.querySelector('div#share')
+
+    if(share.style.display === "none"){
+        share.style.display = "block"
+    }else{
+        share.style.display = "none"
+    }
+    
+}
